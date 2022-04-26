d5ecbdc1aecb623b028572e3abd5779329d50848
scottlexium <scottlexium@gmail.com>
2022-04-26 19:16:12 +0100

    first commit

---
+++ seek.js
@@ -0,0 +1,24 @@
+exports.seekJs = function() {
+    const pwdType = document.querySelector('.toggle_pwd');
+        const pwdInput = document.querySelector('.password_input')
+        const loadValidate = pwdType.checked;
+        window.addEventListener('load', (e)=>{
+            loadValidate?console.log('Text mode'):console.log('pwd mode');
+        })
+        pwdType.addEventListener('click', changeType);
+        function changeType() {
+            const clickValidate = pwdType.checked;
+            switch (clickValidate) {
+                case true:
+                    console.log("Text Mode");
+                    pwdInput.type = 'text';
+                    break;
+                case false:
+                console.log("Password mode");
+                pwdInput.type = 'Password';
+                default:
+                console.log("Hidden as default ");
+                    break;
+            }
+        }
+  }
